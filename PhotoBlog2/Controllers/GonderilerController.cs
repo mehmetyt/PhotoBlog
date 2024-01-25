87333e3dc054868607b46971c9e0729f954550a0
mehme <mehmetyigit.mail@gmail.com>
2024-01-25 09:28:02 +0300

    Eksikler tamamlandi

--- PhotoBlog2/Controllers/GonderilerController.cs
+++ PhotoBlog2/Controllers/GonderilerController.cs
@@ -32,15 +32,91 @@ namespace PhotoBlog2.Controllers
 					vm.Resim.CopyTo(fs);
 				}
 
-				_db.Gonderiler.Add(new Gonderi() 
-				{ 
-					Baslik = vm.Baslik, 
-					ResimYolu = yeniDosyaAd 
+				_db.Gonderiler.Add(new Gonderi()
+				{
+					Baslik = vm.Baslik,
+					ResimYolu = yeniDosyaAd
 				});
 
 				_db.SaveChanges();
 
 				return RedirectToAction("Index", "Home", new { Sonuc = "Eklendi" });
+			}
+
+			return View();
+		}
+
+		public IActionResult Guncelle(int id)
+		{
+			Gonderi gonderi = _db.Gonderiler.Find(id);
+			if (gonderi == null)
+				return NotFound();
+
+
+			return View(new GuncelleGonderiViewModel()
+			{
+				Id = gonderi.Id,
+				Baslik = gonderi.Baslik,
+			});
+		}
+		[HttpPost]
+		public IActionResult Guncelle(GuncelleGonderiViewModel vm)
+		{
+			if (ModelState.IsValid)
+			{
+				Gonderi gonderi = _db.Gonderiler.Find(vm.Id)!;
+				if (gonderi == null)
+					return NotFound();
+				gonderi.Baslik = vm.Baslik;
+
+				if (vm.Resim != null)
+				{
+					string eskiResim = gonderi.ResimYolu;
+
+					string ext = Path.GetExtension(vm.Resim.FileName);
+					string yeniDosyaAd = Guid.NewGuid() + ext;
+
+					gonderi.ResimYolu = yeniDosyaAd;
+
+					string yol = Path.Combine(_env.WebRootPath, "img", "upload", yeniDosyaAd);
+
+					using (var fs = new FileStream(yol, FileMode.CreateNew))
+					{
+						vm.Resim.CopyTo(fs);
+					}
+					string dosya = Path.Combine(_env.WebRootPath, "img", "upload", eskiResim);
+					System.IO.File.Delete(dosya);
+
+				}
+
+				_db.Update(gonderi);
+				_db.SaveChanges();
+
+				return RedirectToAction("Index", "Home", new { Sonuc = "Guncellendi" });
+			}
+
+			return View();
+		}
+
+		public IActionResult Sil(int id)
+		{
+			return View(_db.Gonderiler.Find(id));
+		}
+		[HttpPost]
+		public IActionResult Sil(Gonderi vm)
+		{
+			Gonderi gonderi = _db.Gonderiler.Find(vm.Id);
+			if (gonderi == null)
+				return NotFound();
+
+			if (gonderi != null)
+			{
+				string dosya = Path.Combine(_env.WebRootPath, "img", "upload", gonderi.ResimYolu);
+				System.IO.File.Delete(dosya);
+
+				_db.Gonderiler.Remove(gonderi);
+				_db.SaveChanges();
+				return RedirectToAction("Index", "Home", new { Sonuc = "Silindi" });
 			}
 
 			return View();
---
+++ PhotoBlog2/Models/GuncelleGonderiViewModel.cs
@@ -0,0 +1,18 @@
+﻿using PhotoBlog2.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhotoBlog2.Models
+{
+	public class GuncelleGonderiViewModel
+	{
+        public int Id { get; set; }
+
+        [Display(Name = "Başlık")]
+		[Required(ErrorMessage = "{0} girilmesi zorunludur.")]
+		public string Baslik { get; set; } = null!;
+
+		[Display(Name = "Resim")]
+		[GecerliResim(MaxDosyaBoyutuMb = 1.2)]
+		public IFormFile? Resim { get; set; } = null!;
+	}
+}
